c5074fae6d702fe95a81c283557657fbab8cceb9
shanto150 <44544099+shanto150@users.noreply.github.com>
2023-10-02 11:02:29 +0600

    jb

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -173,7 +173,7 @@
 
                     </div>
                 </form>
-
+Joy Bangla
 
                 <p class="mb-1">
                     <a href="{{ route('password.request') }}">I forgot my password</a>
