f3aa2dacf0a51b2390d8ca95328cb5d6c0a6021e
shanto150 <shanto150@gmail.com>
2023-10-12 20:23:04 +0600

    sec

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Home</title>
 
     <link rel="icon" href="{{ url('/resources/images/appimages/lo.png') }}">
@@ -47,11 +48,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dmuy/MDTimePicker@v1.0.2-rc2/mdtimepicker.min.css">
 
 
-
-    <link
-        href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
-        rel="stylesheet" type="text/css">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
     @stack('style')
