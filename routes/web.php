8ef5aad027eeba747b48a4647b1abb7c497bc511
shanto150 <shanto150@gmail.com>
2023-10-12 19:42:00 +0600

    Tasks

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/routecache', function () {
 });
 
 Route::get('/migrate', function () {
-    Artisan::call('route:migrate');
+    Artisan::call('migrate');
 
     return 'Migrated';
 });
