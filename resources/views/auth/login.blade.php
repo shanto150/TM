f2522d1456747a553eda29cb4737f0f688292d75
shanto150 <shanto150@gmail.com>
2023-10-17 16:30:42 +0600

    new Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
@@ -17,7 +17,39 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 
+    <link rel="stylesheet" href="{{ asset('/resources/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/resources/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/resources/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/fonts/icomoon/style.css') }}">
+
     <style>
+        .has-search .form-control {
+            padding-left: 2.375rem;
+        }
+
+        .form-control:focus {
+            border-color: #34328F;
+            box-shadow: none;
+            -webkit-box-shadow: none;
+        }
+
+        .has-error .form-control:focus {
+            box-shadow: none;
+            -webkit-box-shadow: none;
+        }
+
+        .has-search .form-control-feedback {
+            position: absolute;
+            z-index: 2;
+            display: block;
+            width: 2.375rem;
+            height: 2.375rem;
+            line-height: 2.375rem;
+            text-align: center;
+            pointer-events: none;
+            color: #aaa;
+        }
+
         .con {
             display: block;
             position: relative;
@@ -106,84 +138,89 @@
             -webkit-text-fill-color: transparent;
         }
     </style>
+</head>
 
-<body class="hold-transition login-page">
+<body>
 
-    <div class="login-box">
-        {{-- <img src="{{ url('/resources/images/appimages/mainlogo.png') }}" height="80" width="80" class="rounded mx-auto d-block" alt="..."> --}}
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <span class="sh1">LOGIN</span>
+
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="text-center mb-3">
+                        <img src="{{ url('/resources/images/appimages/galaxylogo.svg') }}" alt="Image" height="80" width="120" class="img-fluid">
+                    </div>
+                    <div class="text-center">
+                        <span class="sh1 mb-3">Task Management</span>
+                    </div>
+                    <img src="{{ url('/resources/images/appimages/undraw_remotely_2j6y.svg') }}" alt="Image"
+                        class="img-fluid">
+                </div>
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="mb-4">
+                                <span class="sh1">LOGIN</span>
+                                <p class="mb-4">Enter Your Login Details</p>
+                            </div>
+
+                            @php
+                                if (isset($_COOKIE['login_email']) && isset($_COOKIE['login_pass'])) {
+                                    $login_email = $_COOKIE['login_email'];
+                                    $login_pass = $_COOKIE['login_pass'];
+                                    $is_remember = "checked='checked'";
+                                } else {
+                                    $login_email = '';
+                                    $login_pass = '';
+                                    $is_remember = '';
+                                }
+                            @endphp
+
+
+                            <form action="{{ route('login') }}" method="post">
+                                @csrf
+                                <div class="form-group has-search">
+                                    <span class="fa fa-user form-control-feedback"></span>
+                                    <input id="email" type="email" placeholder="Email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ request()->input('email', old('email', $login_email)) }}"
+                                        autocomplete="email" autofocus>
+                                </div>
+
+                                <div class="form-group has-search">
+                                    <span class="fa fa-key form-control-feedback"></span>
+                                    <input id="password" type="password" placeholder="Password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        value="{{ $login_pass }}" autocomplete="current-password">
+                                </div>
+
+
+                                <div class="d-flex mb-5 align-items-center">
+                                    <label class="control control--checkbox mb-0"><span class="caption">Remember</span>
+                                        <input type="checkbox" name="remember" id="remember" class="form-control"
+                                            {{ $is_remember }}>
+                                        <div class="control__indicator"></div>
+                                    </label>
+                                    <span class="ml-auto"><a href="{{ route('password.request') }}"class="forgot-pass">Forgot Password</a></span>
+
+                                    <span class="ml-auto"><a href="{{ route('register') }}" class="forgot-pass">Register</a></span>
+
+
+
+                                </div>
+
+                                <input type="submit" value="Log In" style="background: #34328F" class="btn btn-block btn-primary hvr-grow">
+
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
-            <div class="card-body">
-
-                @php
-                    if (isset($_COOKIE['login_email']) && isset($_COOKIE['login_pass'])) {
-                        $login_email = $_COOKIE['login_email'];
-                        $login_pass = $_COOKIE['login_pass'];
-                        $is_remember = "checked='checked'";
-                    } else {
-                        $login_email = '';
-                        $login_pass = '';
-                        $is_remember = '';
-                    }
-                @endphp
-
-                <form action="{{ route('login') }}" method="post">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                        <input id="email" type="email" placeholder="Email"
-                            class="form-control @error('email') is-invalid @enderror" name="email"
-                            value="{{ request()->input('email', old('email', $login_email)) }}" autocomplete="email"
-                            autofocus>
-
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                        <input id="password" type="password" placeholder="Password"
-                            class="form-control @error('password') is-invalid @enderror" name="password"
-                            value="{{ $login_pass }}" autocomplete="current-password">
-
-                    </div>
-
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" name="remember" id="remember" class="form-control"
-                                    {{ $is_remember }}>
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-
-                        <div class="col-4">
-                            <button type="submit" id="SubmitBtn" class="btn btn-primary hvr-grow">Sign In</button>
-                        </div>
-
-                    </div>
-                </form>
-
-                <p class="mb-1">
-                    <a href="{{ route('password.request') }}">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
-                </p>
-            </div>
-
         </div>
-
     </div>
 
 
@@ -192,57 +229,15 @@
     <script src="{{ asset('/resources/js/adminlte.min.js') }}"></script>
     <script src="{{ asset('/resources/js/myfunctions.js') }}"></script>
 
+    <script src="{{ asset('/resources/js/popper.min.js') }}"></script>
+    <script src="{{ asset('/resources/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/resources/js/main.js') }}"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js"></script>
     <script src="https://unpkg.com/tilt.js@1.2.1/dest/tilt.jquery.min.js"></script>
 
-    <script>
-        $('#SubmitBtn').click(function() {
-
-            var vEmail = document.getElementById('email').value;
-            if (vEmail == '') {
-                message('Password Field is Empty!', '#FF0000', 'white', 'error');
-                document.getElementById("email").focus();
-                return false;
-            }
-
-            var vPass = document.getElementById('password').value;
-            if (vPass == '') {
-                message('Password Field is Empty!', '#FF0000', 'white', 'error');
-                document.getElementById("password").focus();
-                return false;
-            }
-
-            return true;
-
-        });
-
-        //logo animation
-        $('#ln').tilt({
-            scale: 1.2
-        })
-
-        //show any error
-        @if (!empty($errors->all()))
-            @foreach ($errors->all() as $eerror)
-                message("{{ $eerror }}", '#FF0000', 'white', 'error');
-            @endforeach
-        @endif
-
-        //show hide password
-        const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#password");
-        togglePassword.addEventListener("click", function() {
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-            this.classList.toggle("bi-eye");
-        });
-        const form = document.querySelector("form");
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-        });
-    </script>
 
 </body>
 
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
         {{-- <img src="{{ asset('') }}resources/images/appimages/spin.png" alt="AdminLTE Logo" id="spn" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light text-center sh1">HOME</span> --}}
         <div style="margin-left: 70px;" class="sh1">
-            HOME
+            TASK
         </div>
     </a>
 
